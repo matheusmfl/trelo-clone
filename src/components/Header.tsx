21b82be64c57d571a5612e895e2b69dc57659e6c
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-05 14:11:22 -0300

    Feat: Create Head COmponent, create Avatar Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Image from 'next/image'
+import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Avatar from 'react-avatar'
+
+export function Header() {
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl md:justify-between">
+        <Image
+          src="https://links.papareact.com/c2cdd5"
+          alt="Trello logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-64 pb-10 md:pb-0 object-contain"
+        />
+
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Buscar"
+              className="flex-1 outline-none p-2"
+            />
+            <button type="submit" hidden>
+              Buscar
+            </button>
+          </form>
+
+          <Avatar name="Matheus Fonteles" round size="50" color="#0055D1" />
+        </div>
+      </div>
+    </header>
+  )
+}
