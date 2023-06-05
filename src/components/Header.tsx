1f3cec05e86fa6b43851cebea5e36f29c75d4100
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-05 16:16:17 -0300

    Fineshed Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,22 @@ export function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl md:justify-between">
+        <div
+          className="absolute
+         top-0
+         left-0
+         w-full
+         h-96
+         bg-gradient-to-br
+         from-pink-400
+         to-[#0055D1]
+         rounded-md
+         filter
+         blur-3xl
+         opacity-50
+         -z-50
+         "
+        />
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="Trello logo"
@@ -30,6 +46,16 @@ export function Header() {
 
           <Avatar name="Matheus Fonteles" round size="50" color="#0055D1" />
         </div>
+      </div>
+
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p
+          className="flex p-5 items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic
+        max-w-3xl text-[#0055d1]"
+        >
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          Chat GPT está resumindo suas tasks de hoje
+        </p>
       </div>
     </header>
   )
